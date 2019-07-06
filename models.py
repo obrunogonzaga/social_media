fdc2318b06f1c7c9639d9358120c897ae4a80b0a
Bruno Santos <brunog86@gmail.com>
2019-07-05 21:51:41 -0300

    Inclur metódo 'create_user' para criar usuário

--- models.py
+++ models.py
@@ -16,3 +16,15 @@ class User(UserMixin ,Model):
     class Meta:
         database = DATABASE
         order_by = ('-joined_at',)
+
+    @classmethod
+    def create_user(cls, username, email, password, admin=False):
+        try:
+            cls.create(
+                username=username,
+                email=email,
+                password=generate_password_hash(password),
+                is_admin=admin
+            )
+        except IntegrityError:
+            raise ValueError('User already exists')
